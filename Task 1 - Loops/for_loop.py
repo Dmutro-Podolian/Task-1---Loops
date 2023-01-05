b541d181d1a039bba4b532d26577d2346f672f35
Dmutro-Podolian <118061088+Dmutro-Podolian@users.noreply.github.com>
2023-01-05 12:18:50 +0200

    Task_loop

--- Task 1 - Loops/for_loop.py
+++ Task 1 - Loops/for_loop.py
@@ -2,12 +2,6 @@ quantity = input("Введіть кількість: ")
 surname = input("прізвище: ")
 intquantity = int(quantity)
 score = 1
-while score <= intquantity:
-  print(score, surname)
-  score += 1
-score = 1
 for intquantity in range(0, intquantity):
   print(score, surname)
   score += 1
-
-
---
+++ Task 1 - Loops/while_loop.py
@@ -0,0 +1,10 @@
+quantity = input("Введіть кількість: ")
+surname = input("прізвище: ")
+intquantity = int(quantity)
+score = 1
+while score <= intquantity:
+  print(score, surname)
+  score += 1
+
+
+
